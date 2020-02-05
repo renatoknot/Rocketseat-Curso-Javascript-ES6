70c6a7fbd36cb01541e2460d69516949614321f9
Renato Gonçalves <renatoslip@hotmail.com>
2020-02-05 19:38:44 -0300

    modulo 4

--- src/main.js
+++ src/main.js
@@ -12,9 +12,9 @@
 
 //  console.log(funcoes.soma(1,2));
 //  console.log(funcoes.sub(1,2));
-const minhaPromise = () => new Promise((resolve, reject) => {
-    setTimeout(() => {resolve('OK')}, 2000);
-});
+// const minhaPromise = () => new Promise((resolve, reject) => {
+//     setTimeout(() => {resolve('OK')}, 2000);
+// });
 
 // minhaPromise().then(response => ...); ANTES
 
@@ -47,14 +47,42 @@ const minhaPromise = () => new Promise((resolve, reject) => {
 
 // executaPromise();
 
-import axios from 'axios';
+// import axios from 'axios';
 
-class Api {
-    static async getUserInfo(username){
-        const response = await axios.get(`https://api.github.com/users/${username}`);
+// class Api {
+//     static async getUserInfo(username){
+//         const response = await axios.get(`https://api.github.com/users/${username}`);
 
-        console.log(response);
+//         console.log(response);
+//     }
+// }
+
+// Api.getUserInfo('renatoknot');
+
+class App {
+    constructor() {
+        this.repositories = [];
+        
+        this.formEl = document.getElementById('repo-form');
+
+        this.registerHandlers();
+    }
+    registerHandlers(){
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    addRepository(){
+        event.preventDefault();//evita recarregar html
+
+        this.repositories.push({
+            name: 'rocketseat.com.br',
+            description: 'Tire a sua ideia do papel e dê vida à sua startup.',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'http://github.com/rocketseat/rocketseat.com.br'
+        });
+
+        console.log(this.repositories);
     }
 }
 
-Api.getUserInfo('renatoknot');
+new App();
